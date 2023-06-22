6ef02fee6b948f177f319d089730f1118154ae4e
carmela-nicole <bautistacarmelanicole@gmail.com>
2023-06-22 09:49:10 +0800

    Controllers

---
+++ app/Http/Controllers/EnhypenController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EnhypenController extends Controller
+{
+    public function index(){
+        return view('welcome');
+        // return view('welcome');
+        //              'name of the blade"
+
+    }
+}
