cc4be03aff9bb2ebd148d75c9d849810a4387cd9
carmela-nicole <bautistacarmelanicole@gmail.com>
2023-06-22 09:48:57 +0800

    5 blades

---
+++ resources/views/albums.blade.php
@@ -0,0 +1,11 @@
+@extends('template.app')
+
+@section('content')
+<h1> ALBUMS </h1>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque, culpa nihil natus repellendus blanditiis reprehenderit officiis fuga dolorem vel quod omnis sequi, reiciendis asperiores nesciunt, magnam quisquam cum vitae dolores.</p>
+
+@endsection
+
+@section('title')
+<h1>Title</h1>
+@endsection
